fef444067bcdf08475313e2938f1ccabb0887532
MathewT16 <mathewtata2003@gmail.com>
2022-11-03 11:27:45 -0400

    Update double_int_list_practice.py

--- Assignment 4/blackjack.py
+++ Assignment 4/blackjack.py
@@ -3,18 +3,20 @@
 '''instructions: randomly generate three values between 1 and 11. in the function bust: add these three numbers together. if they add up to or less than 21, return the sum. If it's over 21, return 0. If it's over 21 BUT there's an 11 as one of the values, return the sum - 10. '''
 import random
 
-
+#adds the parameters than if check the value of total and depending on what it is return a certain number
 def bust(num1,num2,num3):
-    tot = num1 + num2 + num3
-    if tot <= 21:
-        return tot
-    elif tot > 21:
+    totNum = num1 + num2 + num3
+    if totNum <= 21:
+        return totNum
+    elif totNum > 21:
        if num1 == 11 or num2 == 11 or num3 == 11:
-            return tot - 10
-       return 0
+            return totNum - 10
+    return 0
 
     
-    
+
+
+#creates 3 random variables then calls and prints bust
 def main():
     num1 = int(random.random() * 11) + 1
     # or random.randint(1,11)
