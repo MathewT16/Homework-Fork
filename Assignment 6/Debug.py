f41bc9c33444103bfaaa5a000fbc18fca86b1238
MathewT16 <mathewtata2003@gmail.com>
2022-11-10 13:33:14 -0500

    Update double_int_list_practice.py

--- Assignment 6/Debug.py
+++ Assignment 6/Debug.py
@@ -12,21 +12,26 @@ class Employee:
 class Cake:
     #can you fill out the rest of this for me? im dumb
     #the cake needs to have the cake flavor and cake frosting stored
+    def __init__(self,flavor,frosting_Stored):
+        self.flavor = flavor
+        self.frosting_Stored = frosting_Stored
         
 def main():
     #fill this one out with a dog's name and age.. can be your dog, friend's dog, etc
-    newDog = Dog(? , ?)
-    print(dog1.name, dog1.age)
+    newDog = Dog("Scooby Doo" , 62)
+    print(newDog.name, newDog.age)
     
     #and what about a new employee
-    newEmployee =
+    newEmployee = Employee("Joe Biden", 46, "Presidency")
     #how would we print out each of the variables from newEmployee?
-    print()
+    print(newEmployee.name,newEmployee.idNumber,newEmployee.department)
     
     #now create and print out a cake you make
-    
+    vanillaCake = Cake("Vanilla",100)
+    print(vanillaCake.flavor,vanillaCake.frosting_Stored)
     
     
     #and now create another cake and print it out
-
+    bloodCake = Cake("Blood", 100000)
+    print(bloodCake.flavor, bloodCake.frosting_Stored)
 main()
